647308475ba62b54a629f01ba67221cb5b1df447
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2023-08-06 15:17:13 +0500

    Two tables with Hibernate

--- src/main/java/com/example/daolayer/repository/DAORepository.java
+++ src/main/java/com/example/daolayer/repository/DAORepository.java
@@ -1,46 +1,25 @@
 package com.example.daolayer.repository;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 public class DAORepository {
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    private String pathToScript = "ProductNames.sql";
-
-    public DAORepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public List<String> getProductName(String customerName) {
-        MapSqlParameterSource parametersMap = new MapSqlParameterSource();
-        parametersMap.addValue("name", customerName);
-        String script = read(pathToScript);
-        return namedParameterJdbcTemplate.queryForList(
-                script, parametersMap, String.class
-        );
+        String script = "SELECT o.productName FROM orders o " +
+                "WHERE o.customer.id IN (SELECT c.id FROM customers c WHERE LOWER(c.name) = LOWER(:customerName))";
+
+        return entityManager.createQuery(script, String.class)
+                .setParameter("customerName", customerName)
+                .getResultList();
+
     }
 
-    private static String read(String scriptFileName) {
-        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
-             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
-            return bufferedReader.lines().collect(Collectors.joining("\n"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    public void changeScriptPath(String path) {
-        pathToScript = path;
-    }
 }
